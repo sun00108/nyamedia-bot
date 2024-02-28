3555c5f8bf2802e1b90eaa77c5d9a4d20f39ab73
sun00108 <sun00108@acgn.ca>
2024-02-28 00:54:58 -0600

    bug fix: 私聊 /help 回复帮助信息。

--- src/bot.rs
+++ src/bot.rs
@@ -62,32 +62,39 @@ fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>>
 }
 
 async fn help(bot: Bot, msg: Message) -> HandlerResult {
-    match msg.kind {
-        MessageKind::Common(common) => {
-            if let Some(user) = common.from {
-                // Anti Spam Module - 防止某些特定的人使用这个命令
-                let disabled_users = env::var("DISABLED_USERS");
-                let disabled_users = match disabled_users {
-                    Ok(disabled_users) => {
-                        disabled_users.split(",").map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>()
-                    },
-                    Err(_) => {
-                        Vec::new()
+    match msg.chat.kind {
+        ChatKind::Public(_) => {
+            match msg.kind {
+                MessageKind::Common(common) => {
+                    if let Some(user) = common.from {
+                        // Anti Spam Module - 防止某些特定的人使用这个命令
+                        let disabled_users = env::var("DISABLED_USERS");
+                        let disabled_users = match disabled_users {
+                            Ok(disabled_users) => {
+                                disabled_users.split(",").map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>()
+                            },
+                            Err(_) => {
+                                Vec::new()
+                            }
+                        };
+                        if disabled_users.contains(&user.id.0) {
+                            bot.send_message(msg.chat.id, "抱歉，您没有权限使用这个命令。").await?;
+                            return Ok(());
+                        } else {
+                            let sent_msg = bot.send_message(msg.chat.id, "请查看群简介。").await?;
+                            tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+                            bot.delete_message(msg.chat.id, msg.id).await?;
+                            bot.delete_message(msg.chat.id, sent_msg.id).await?;
+                        }
                     }
-                };
-                if disabled_users.contains(&user.id.0) {
-                    bot.send_message(msg.chat.id, "抱歉，您没有权限使用这个命令。").await?;
-                    return Ok(());
-                } else {
-                    let sent_msg = bot.send_message(msg.chat.id, "请查看群简介。").await?;
-                    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-                    bot.delete_message(msg.chat.id, msg.id).await?;
-                    bot.delete_message(msg.chat.id, sent_msg.id).await?;
+                }
+                _ => {
+                    bot.send_message(msg.chat.id, "未知错误，请联系管理员。[Command::Help]").await?;
                 }
             }
         }
         _ => {
-            bot.send_message(msg.chat.id, "未知错误，请联系管理员。[Command::Help]").await?;
+            bot.send_message(msg.chat.id, "可用命令：\n/help - 显示此帮助\n/register - 注册新用户").await?;
         }
     }
     Ok(())
